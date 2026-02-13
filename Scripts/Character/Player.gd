a0d93daa467ab56007ba54e8918cc6dbdb14522b
ROJON Louise <louise.rojon@groupe-sra.lan>
2026-02-13 17:21:21 +0100

    Start airplane physics and control

--- Scripts/Character/Player.gd
+++ Scripts/Character/Player.gd
@@ -1,25 +1,36 @@
-extends CharacterBody2D
+extends RigidBody2D
 
+##### DECLARATIONS #####
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var deathCollision: Area2D = $DeathCollision
+@onready var vx = $UI/Control/VX
+@onready var vy = $UI/Control/VY
+@onready var r = $UI/Control/R
 
+@export var SPEED = 300.0
+var n = 0
+##### BUILT-IN #####
+
+func _ready() -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+		#self.linear_velocity += get_gravity()
+	#else:
+		#self.linear_velocity.y = 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("Accelerate"):
+		self.linear_velocity.x += SPEED
+	
+	vx.text = "Vitesse X : " + str(snappedf(linear_velocity.x, 0.01))
+	vy.text = "Vitesse Y : " + str(snappedf(linear_velocity.y, 0.01))
+	r.text = "Rotation : " + str(snappedf(rotation_degrees, 0.01))
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+##### LOGIC #####
+
+func is_on_floor() -> bool:
+	return self.get_contact_count() > 0
+
+##### SIGNAL RESPONSES #####
